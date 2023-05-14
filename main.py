f306d2dbf60979d7a3bf6863c3479cf3e81b9470
Leo Lira <liradiniz89@gmail.com>
2023-05-14 09:45:04 -0300

    Start the repo, put the temperature measure

---
+++ main.py
@@ -0,0 +1,12 @@
+import os
+import time
+
+# def postTemperature():
+    
+
+
+    # bot.post("XMechina Bot, on "+str(data)+". Current CPU temperature: " + temp)
+
+print("Recueprando Temperatura...")
+temp = os.popen("/opt/vc/bin/vcgencmd measure_temp").read().split("=")[1]
+data = time.ctime()
